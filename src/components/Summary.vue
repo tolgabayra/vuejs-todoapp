3a5e1f1a03e836cbfa656f13d442dcf30192aad4
Tolga Bayrak <93285625+tolgabayrakx@users.noreply.github.com>
2022-01-20 11:58:30 +0300

    Add files via upload

---
+++ src/components/Summary.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="text-green-300">You have {{completed}} completed tasks </div>
+    <div class="text-red-300">You have {{unCompleted}} uncompleted tasks </div>
+</template>
+
+<script>
+import {computed} from "vue"
+export default {
+    props : {
+        todoList : Array,
+
+    },
+    setup(props){
+        const completed = computed(()=> props.todoList.filter((i) => i.completed).length)
+        const unCompleted = computed(()=> props.todoList.filter((i) => !i.completed).length)
+
+
+        return {completed, unCompleted}
+    }
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode : "jit",
+  content: ["./src/**/*.vue"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
